c26f4aa6ea2b28d061c01de690effe4f68a60e6d
mpilkou <mpilkou@gmail.com>
2022-11-05 03:22:34 +0300

    add admin.py

--- task1_project/api/admin.py
+++ task1_project/api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
+
+from django.utils.html import format_html
+from django.urls import reverse
+
 
 # Register your models here.
+class ChainInline(admin.StackedInline):
+    model = Chain
+class ContactsInline(admin.StackedInline):
+    model = Contacts
+class ChainAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'name', 'type', 'debt', )
+    
+    list_display_links = ('name', )
+
+    list_filter = ('contacts__city', )
+
+
+    inlines = [
+        ChainInline,
+        ContactsInline,
+    ]
+
+admin.site.register(Contacts)
+admin.site.register(Chain, ChainAdmin)
