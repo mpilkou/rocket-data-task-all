57c89a636a813535e63c6b400de1e9c571377803
mpilkou <mpilkou@gmail.com>
2023-03-02 00:37:01 +0300

    Update admin.py

--- task1_project/api/admin.py
+++ task1_project/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
+import multiprocessing
+import asyncio
 from api.models import (
     Chain,
     Contact,
@@ -25,17 +27,49 @@ class ChainAdmin(admin.ModelAdmin):
 
     actions = ('clear_debt', )
 
+    def async_clear_20_debt():
+        pass
+        
+        def square(n):
+            return (n*n)
+        
+        if __name__ == "__main__":
+            # input list
+            mylist = [1,2,3,4,5]
+        
+            # creating a pool object
+            p = multiprocessing.Pool()
+        
+            # map list to target function
+            result = p.map(square, mylist)
+        
+            print(result)
+
     def view_supplier_link(self, obj):
         url = reverse("admin:api_chain_change", args=[obj.supplier_id, ])
         return format_html('<a href="{}"> {} </a>', url, obj.supplier_id)
     view_supplier_link.short_description = "Supplier"
 
-    def clear_debt(self, req, queryset):
-        for chain in queryset:
-            if chain.debt > 0:
-                chain.debt = 0
-                chain.save()
-        self.message_user(req, "Debt is cleaned")
+    def clear_debt(self, _, queryset):
+        devision = 20
+        query_len = queryset.count()
+        if query_len > devision:
+            pool_len_number = query_len // devision
+            pool_len_number = pool_len_number if query_len % devision == 0 else pool_len_number+1
+            pool = multiprocessing.Pool(processes=pool_len_number)
+            # array = multiprocessing.Array(type(Chain), query_len)
+            # for part in range(pool_len_number):
+            for part in range(0, query_len, devision):
+                pass
+                # multiprocessing.Array()
+                
+
+        else:
+            for chain in queryset:
+                if chain.debt > 0:
+                    chain.debt = 0
+                    chain.save()
+        # self.message_user(req, "Debt is cleaned")
 
     clear_debt.description = 'Clear debt'
     inlines = [
